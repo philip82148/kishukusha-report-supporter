fcfcb5a9d0c430f2cf9d417cd5aea0122c6c1f31
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-05-20 12:39:38 +0900

    create config-selector.php

--- includes.php
+++ includes.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/config-selector.php';
 require_once __DIR__ . '/manuals.php';
 
 require_once __DIR__ . '/functions/check-signature.php';
